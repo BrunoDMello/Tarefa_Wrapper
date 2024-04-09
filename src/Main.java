f198a434cf42ff6ae1d54ed66169678e4c358e8e
BrunoDMello <bruno.ernst.mello.dev@gmail.com>
2024-04-09 19:14:17 -0300

    Subindo tarefa_wrapper

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @author bruno
+ */
+
+public class Main {
+
+    public static void main(String args[]) {
+
+     int num1 = 1234;
+
+     System.out.println("Variavel primitiva int:" + num1);
+
+    Integer num2 = (Integer) num1;
+
+     System.out.println("Variavel wrapper Integer:" + num2);
+
+    }
+}
